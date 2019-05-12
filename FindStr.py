914680f941d54241d71d881314f5c08d0f11af53
Cuba <serp2007@yandex.ru>
2019-05-12 20:08:24 +0300

    Первый коммит

---
+++ FindStr.py
@@ -0,0 +1,24 @@
+
+import sys
+
+print("Number of arguments: ", len(sys.argv))
+print("This is the name of the script: ", sys.argv[0])
+print("The arguments are: " , str(sys.argv))
+if (len(sys.argv) < 3):
+    exit("arguments not found")
+
+str1 = sys.argv[2]
+str2 = sys.argv[1]
+
+if (str1.find(str2) >= 0):
+    print("строка нашлась")
+    exit(0)
+elif (str2.find(str1) >= 0):
+    print("строка нашлась")
+    exit(0)
+elif str1==str2:
+    print("строка нашлась")
+    exit(0)
+else:
+    print("строка не нашлась")
+    exit(1)
